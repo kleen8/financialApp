b90ebe5a5036ead561272a21e26d79a8bd7f98c4
Jelle Jacobs <jjacobs@sogyo.nl>
2024-12-23 11:07:38 +0100

    adding java files to project

---
+++ persistence/src/main/java/nl/sogyo/financialApp/App.java
@@ -0,0 +1,13 @@
+package nl.sogyo.financialApp;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
