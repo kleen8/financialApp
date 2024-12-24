63c24def16d2784b946871c2d28b56a41c65d918
Jelle Jacobs <jjacobs@sogyo.nl>
2024-12-24 14:01:02 +0100

    this is the code for testing the persistence in the docker container

--- persistence/src/main/java/nl/sogyo/financialApp/DatabaseConnection.java
+++ persistence/src/main/java/nl/sogyo/financialApp/DatabaseConnection.java
@@ -4,6 +4,8 @@ import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.SQLException;
 
+import org.postgresql.Driver;
+
 public class DatabaseConnection {
 
     public static Connection getConnection() throws SQLException {
