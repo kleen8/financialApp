cd7b097fbe4e86bf710e82bbbe2f3a53be280376
Jelle Jacobs <jjacobs@sogyo.nl>
2025-01-08 15:49:07 +0100

    Adding log dep to the persistence pom and updated 1 method with the logger

--- persistence/src/main/java/nl/sogyo/financialApp/UserDao.java
+++ persistence/src/main/java/nl/sogyo/financialApp/UserDao.java
@@ -1,5 +1,8 @@
 package nl.sogyo.financialApp;
 
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+
 import java.sql.*;
 import java.util.ArrayList;
 import java.util.HashMap;
@@ -12,6 +15,12 @@ import nl.sogyo.financialApp.exception.UserNotFoundException;
 import nl.sogyo.financialApp.exception.AuthenticationException;
 
 public class UserDao implements IUserDAO{
+
+    private static final Logger LOGGER = LogManager.getLogger(UserDao.class);
+
+    private static final Logger SQL_LOGGER = LogManager.getLogger("SQLExceptionLogger");
+
+
 
     private final String saveUserQuery = """
     INSERT INTO users (email, first_name, last_name,
@@ -26,7 +35,7 @@ public class UserDao implements IUserDAO{
     private final String findAllUsers = """
     SELECT * FROM users;
     """;
-    
+
     private final String deleteUser = """
     DELETE FROM users WHERE email = ?;
     """;
@@ -34,17 +43,17 @@ public class UserDao implements IUserDAO{
     private final String updateUserEmailQry = """
     UPDATE users 
     SET first_name = COALESCE(?, first_name),
-        last_name = COALESCE(?, last_name),
-        street_name = COALESCE(?, street_name),
-        zip_code = COALESCE(?, zip_code),
-        house_number = COALESCE(?, house_number),
-        city = COALESCE(?, city),
-        county = COALESCE(?, country),
-      WHERE email = ?;
+    last_name = COALESCE(?, last_name),
+    street_name = COALESCE(?, street_name),
+    zip_code = COALESCE(?, zip_code),
+    house_number = COALESCE(?, house_number),
+    city = COALESCE(?, city),
+    county = COALESCE(?, country),
+    WHERE email = ?;
     """;
 
-	@Override
-	public void save(User user, String password) {
+    @Override
+    public void save(User user, String password) {
         String passwordHash = hashPassword(password);
         try (Connection connection = DatabaseConnection.getConnection()){
             PreparedStatement stmt = connection.prepareStatement(saveUserQuery);
@@ -63,7 +72,7 @@ public class UserDao implements IUserDAO{
             e.printStackTrace();
             throw new RuntimeException("Database error occured");
         }
-	}
+    }
 
     // because then the frontend needs to get notified
     @Override
@@ -79,10 +88,11 @@ public class UserDao implements IUserDAO{
                     throw new UserNotFoundException("User is not found");
                 }
             }
-            } catch (SQLException e) {
-                e.printStackTrace();
-                throw new RuntimeException("Database error occured");
-            }
+        } catch (SQLException e) {
+            e.printStackTrace();
+            SQL_LOGGER.error("SQLException occured at {}: {}" , java.time.LocalDateTime.now(), e.getMessage());
+            throw new RuntimeException("Database error occured");
+        }
     }
 
 
@@ -92,12 +102,12 @@ public class UserDao implements IUserDAO{
             PreparedStatement stmt = connection.prepareStatement(findUserByEmail);
             stmt.setString(1, email);
             try (ResultSet resultSet = stmt.executeQuery()){
-                    return resultSet.next();
-                }
-            } catch (SQLException e) {
-                e.printStackTrace();
-                return false;
+                return resultSet.next();
             }
+        } catch (SQLException e) {
+            e.printStackTrace();
+            return false;
+        }
     }
 
     public HashMap<String, String> loginUser(String email, String password) {
@@ -112,22 +122,24 @@ public class UserDao implements IUserDAO{
                         Integer userId = resultSet.getInt("id");
                         sessionCredentials.put("userEmail", email);
                         sessionCredentials.put("userId", userId.toString());
+                        LOGGER.info("User found for email: {}", email);
                         return sessionCredentials;
                     } else {
                         throw new AuthenticationException("Authentication incorrect");
                     }
-                 } else {
-                        throw new UserNotFoundException("User with email: " + email + " not found");
+                } else {
+                    LOGGER.warn("User not found for email: {}", email);
+                    throw new UserNotFoundException("User with email: " + email + " not found");
                 }
-              }
-            } catch (SQLException e) {
-                e.printStackTrace();
-                throw new RuntimeException("Database error occured");
             }
+        } catch (SQLException e) {
+            e.printStackTrace();
+            throw new RuntimeException("Database error occured");
+        }
     }
 
     @Override
-	public List<User> findAll() {
+    public List<User> findAll() {
         List<User> users = new ArrayList<>();
         try (Connection connection = DatabaseConnection.getConnection()) {
             PreparedStatement stmt = connection.prepareStatement(findAllUsers);
@@ -141,10 +153,10 @@ public class UserDao implements IUserDAO{
             throw new RuntimeException("Database error occured");
         }
         return users;
-	}
+    }
 
-	@Override
-	public void update(User user) {
+    @Override
+    public void update(User user) {
         try (Connection connection = DatabaseConnection.getConnection()) {
             PreparedStatement stmt = connection.prepareStatement(updateUserEmailQry);
             stmt.setString(1, user.getFirstName());
@@ -160,10 +172,10 @@ public class UserDao implements IUserDAO{
             e.printStackTrace();
             throw new RuntimeException("Database error occured");
         }
-	}
+    }
 
-	@Override
-	public void delete(String email) {
+    @Override
+    public void delete(String email) {
         try (Connection connection = DatabaseConnection.getConnection()) {
             PreparedStatement stmt = connection.prepareStatement(deleteUser);
             stmt.setString(1, email);
@@ -172,18 +184,18 @@ public class UserDao implements IUserDAO{
             e.printStackTrace();
             throw new RuntimeException("Database error occured");
         }
-	}
+    }
 
     protected String hashPassword(String password){
         Hash hash = Password.hash(password)
-                            .withArgon2();
+        .withArgon2();
         return hash.getResult();
     }
 
     protected boolean isPasswordCorrect(String hashedPassword, String password){
         return Password.check(password, hashedPassword).withArgon2();
     }
-    
+
     protected User mapToUser(ResultSet resultSet){
         try {
             String first_name = resultSet.getString("first_name");
@@ -194,16 +206,13 @@ public class UserDao implements IUserDAO{
             String zip_code = resultSet.getString("zip_code");
             String street_name = resultSet.getString("street_name");
             String house_number = resultSet.getString("house_number");
-
             User user = User.createUser(first_name, last_name, email, street_name, zip_code, house_number, city, country);
-
             return user;
         } catch (Exception e) {
             e.printStackTrace();
 
         }
         return null;
-        
     }
 
     public int getUserIdByEmail(String email) {
@@ -218,7 +227,7 @@ public class UserDao implements IUserDAO{
             e.printStackTrace();
             throw new RuntimeException("Database error occured");
         }
-        return -1;
+        throw new UserNotFoundException("User with email: " + email + " not found");
     }
 
 }
