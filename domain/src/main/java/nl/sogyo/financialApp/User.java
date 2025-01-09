2c96d7ecb2cbddf5a081a4ba1ff6a7ec35adda92
Jelle Jacobs <jjacobs@sogyo.nl>
2025-01-09 16:16:57 +0100

    Update User

--- domain/src/main/java/nl/sogyo/financialApp/User.java
+++ domain/src/main/java/nl/sogyo/financialApp/User.java
@@ -1,8 +1,5 @@
 package nl.sogyo.financialApp;
 
-import java.util.ArrayList;
-import java.util.List;
-
 public class User {
 
     protected String firstName;
@@ -13,8 +10,6 @@ public class User {
     protected String houseNumber;
     protected String city;
     protected String country;
-    protected List<Expense> expenses = new ArrayList<Expense>();
-    protected List<Income> incomes = new ArrayList<Income>();
 
     private User(String firstName, String lastName,
             String email, String streetName, String zipCode, String houseNumber,
