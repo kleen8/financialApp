30783422a2e49ef6d73ff41a66ff99d4f38dc225
Jelle Jacobs <jjacobs@sogyo.nl>
2025-01-11 22:10:02 +0100

    Adding UserDTO file, forgotten with last commit

---
+++ api/src/main/java/nl/sogyo/financialApp/controller/DTO/IUserDTO.java
@@ -0,0 +1,14 @@
+package nl.sogyo.financialApp.controller.DTO;
+
+public interface IUserDTO {
+
+    String getFirstName();
+    String getLastName();
+    String getEmailUser();
+    String getStreetName();
+    String getZipCode();
+    String getHouseNumber();
+    String getCity();
+    String getCountry();
+    String getPassword();
+}
